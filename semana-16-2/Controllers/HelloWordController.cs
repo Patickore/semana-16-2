947fc244052c14170e48f2aceafe036b96d7e152
Alumnos <Alumnos@LABC2PC9>
2022-11-11 12:12:04 -0600

    Agregar archivos de proyecto.

---
+++ semana-16-2/Controllers/HelloWordController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+
+namespace semana_16_2.Controllers
+{
+    public class HelloWordController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string Welcome()
+        {
+            return "Esta es la acción de bienvenida";
+        } 
+
+        public string Parametros(string name, int edad)
+        {
+            return HtmlEncoder.Default.Encode($"Hola {name}, tu edad es: {edad} años");
+        }
+    }
+}
